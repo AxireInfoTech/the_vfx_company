747009ff5bc6bcd6ddf6c3f7e513a43cbaa2bf00
RutvikJR <rachhadiyarutvik09040@gmail.com>
2022-07-20 21:08:20 +0530

    slider added in client section, client url changed

--- src/app/components/clients/clients.module.ts
+++ src/app/components/clients/clients.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ClientsComponent } from './clients.component';
-
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [ClientsComponent],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [ClientsComponent]
+  imports: [CommonModule, RouterModule, CarouselModule],
+  exports: [ClientsComponent],
 })
-export class ClientsModule { }
+export class ClientsModule {}
